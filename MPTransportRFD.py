2ab7b3ab12ee5440cf2b0dd6a8caa571428cdacd
Marcela Strane <95592640+marcela-strane@users.noreply.github.com>
2023-03-13 13:43:59 -0500

    added rainfall intensity table

--- MPTransportRFD.py
+++ MPTransportRFD.py
@@ -1,2 +1,7 @@
-import pandas as pd
-from 
+CNVector= [73,75,77,78,79,80,81,82,84,86,87,88,89,90,91,92,93,94,95,97,98]
+RS= (1000/CNVector)-10 #retention max (storage) inches based on CN
+iA=.2*RS #initial abstraction (inches)
+
+#Rainfall Intensity
+
+
