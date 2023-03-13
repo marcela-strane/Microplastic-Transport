dc1e0430c0f4a9b50553e72b56dec511aa0ed2c2
Marcela Strane <95592640+marcela-strane@users.noreply.github.com>
2023-03-13 15:18:11 -0500

    updating rainfall intensities

--- MPTransportRFD.py
+++ MPTransportRFD.py
@@ -1,7 +1,41 @@
-CNVector= [73,75,77,78,79,80,81,82,84,86,87,88,89,90,91,92,93,94,95,97,98]
-RS= (1000/CNVector)-10 #retention max (storage) inches based on CN
-iA=.2*RS #initial abstraction (inches)
+CNVector=[73,75,77,78,79,80,81,82,84,86,87,88,89,90,91,92,93,94,95,97,98];
+#RS=(1000/CNVector)-10; #retention max (storage) inches based on CN
+#iA=.2*RS; #initial abstraction (inches)
 
 #Rainfall Intensity
+!pip install [tabulate]
+from tabulate import tabulate
 
+assign data
+mydata = [
+    ["2-year", "48.35", "9.07", "0.7244"],
+    ["5-year", "52.32", "7.88", "0.69"],
+    ["10-year", "54.68", "6.96", "0.6623"],
+    ["25-year", "57.79", "5.89", "0.6294"],
+    ["50-year", "61", "5.46", "0.6096"],
+    ["100-year", "60.66", "4.44", "0.5797"],
+    ["500-year", "62.17", "2.95", "0.5196"]
+]
+create header
+head= ["Rainfall Intensity", "b", "d", "e"]
+
+display table
+print(tabulate(mydata, headers=head, tablefmt="grid"))
+
+#Calculate rainfall intensity for each frequency
+TC=1140 #time of concentration in minutes
+b1=48.35; b2=52.32; b3=54.68; b4=57.79; b5=61; b6=60.66; b7=62.15;
+d1=9.07; d2=7.88; d3=6.96; d4=5.89; d5=5.46; d6=4.44; d7=2.95;
+e1=0.7244; e2=0.69; e3=0.6623; e4=0.6294; e5=0.6096; e6=0.5797; e7=0.5196;
+twoyi=b1/(d1+TC)**e1
+fiveyi=b2/(d2+TC)**e2
+tenyi=b3/(d3+TC)**e3
+twentyfiveyi=b4/(d4+TC)**e4
+fiftyyi=b5/(d5+TC)**e5
+hundredyi=b6/(d6+TC)**e6
+fivehundredyi=b7/(d7+TC)**e7
+
+print(twoyi, fiveyi, tenyi, twentyfiveyi, fiftyyi, hundredyi, fivehundredyi, sep= "\n")
+
+    
 
