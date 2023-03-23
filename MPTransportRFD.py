2f59ed2fb6b4299c35c28815b3bcc4b9c2899855
Marcela Strane <95592640+marcela-strane@users.noreply.github.com>
2023-03-23 15:06:30 -0500

    fixed the TC value, checked all answers compared to my raw excel file, and started Flow Rate template

--- MPTransportRFD.py
+++ MPTransportRFD.py
@@ -1,4 +1,5 @@
 CNlist=[73,75,77,78,79,80,81,82,84,86,87,88,89,90,91,92,93,94,95,97,98];
+#Finding Retention Max (Storage)
 def RS(array):
     #Creates an empty list
     rs= []
@@ -16,10 +17,11 @@ def RS(array):
 answerArray = RS(CNlist)
 print(f"answer array={answerArray}")
 
-#Initial Abstraction
+#Solving for Initial Abstraction
 c=0.2 #coefficient of I=.2S
 iA = [i * c for i in answerArray]
-print(f'{iA=}')
+print(f'{iA=}') 
+print(f'in')
 
 print("-------------------------------------------------------------------------------------------------)")
 #Rainfall Intensity
@@ -41,8 +43,8 @@ print(tabulate(mydata,headers=head, tablefmt="grid"))
 
 #Calculate rainfall intensity for each frequency using the  IDF curves and Houston IDF Equations
 print("----------------------------------------------------------------")
-TC=1140 #time of concentration in minutes
-
+TC=1440 #time of concentration in minutes
+#solving for intensity using these parameters below
 b1=48.35; b2=52.32; b3=54.68; b4=57.79; b5=61; b6=60.66; b7=62.15;
 d1=9.07; d2=7.88; d3=6.96; d4=5.89; d5=5.46; d6=4.44; d7=2.95;
 e1=0.7244; e2=0.69; e3=0.6623; e4=0.6294; e5=0.6096; e6=0.5797; e7=0.5196;
@@ -54,6 +56,7 @@ fiftyyi=b5/(d5+TC)**e5
 hundredyi=b6/(d6+TC)**e6
 fivehundredyi=b7/(d7+TC)**e7
 
+#intensity isn't printed but P2=i*24hrs
 print("Rainfall Intensity Events:P2 or P") #now print the variables and outputs side by side
 hr=24 #hours of a rainfall event
 #assign the new values as a variables in the dictionary created
@@ -72,6 +75,6 @@ print("                          inches")
 print("--------------------------------------------------------------------------------")
 
 #Flow Rate of the Precipitation P to Q
-
+#Q=((P2-iA)^2)/((P2-iA)+S)
 
 
